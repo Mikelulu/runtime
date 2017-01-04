350001cd919dab7d389b577d54aa341e7a649e5c
杨陆可 <13164355736@163.com>
2017-01-04 16:49:50 +0800

    提交代码

--- Runtime/Runtime/UIView+LKExtension.m
+++ Runtime/Runtime/UIView+LKExtension.m
@@ -9,17 +9,17 @@
 #import "UIView+LKExtension.h"
 #import <objc/runtime.h>
 
-static const char kAnaylizeTitle;
+static const char *kAnaylizeTitle = "kAnaylizeTitle";
 
 @implementation UIView (LKExtension)
 
 - (NSString *)lk_anaylizeTitle
 {
-    return objc_getAssociatedObject(self, &kAnaylizeTitle);
+    return objc_getAssociatedObject(self, kAnaylizeTitle);
 }
 
 - (void)setLk_anaylizeTitle:(NSString *)lk_anaylizeTitle
 {
-    objc_setAssociatedObject(self, &kAnaylizeTitle, lk_anaylizeTitle, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    objc_setAssociatedObject(self, kAnaylizeTitle, lk_anaylizeTitle, OBJC_ASSOCIATION_COPY_NONATOMIC);
 }
 @end
